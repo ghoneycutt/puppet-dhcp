bcda5584baf708cd99298b6820181adb12ae127a
ghoneycutt <garrett@puppetlabs.com>
2011-01-27 17:16:55 -0800

    work in progress - saving for nan to take a look

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,38 +2,47 @@
 #
 # This module manages the DHCP service
 #
+# Requires:
+#   $nameservers be an initialized array in site.pp
+#
 class dhcp {
 
-    package { "dhcp": }
+  # referenced in dhcpd.conf.erb
+  $includes    = "/etc/dhcp"
+  # dhcp host entries generated from CMDB
+  $hostsconfig = "${dhcp::includes}/hosts.conf"
+  # referenced in default_snippet.erb
+  $filename    = "/pxelinux.0"
 
-    # referenced in dhcpd.conf.erb
-    $includes = "/etc/dhcp"
+  package { "dhcp": }
 
-    file {
-        "/etc/dhcpd.conf":
-            content => template("dhcp/dhcpd.conf.erb"),
-            notify  => Service["dhcpd"];
-        "${includes}":
-            ensure  => directory;
-    } # file
+  file {
+    "/etc/dhcpd.conf":
+      content => template("dhcp/dhcpd.conf.erb"),
+      notify  => Service["dhcpd"];
+    "${includes}":
+      ensure  => directory;
+    "${hostsconfig}":
+      content => generatedhcphosts(),
+  } # file
 
-    exec { "prep dhcp_include":
-        command  => "echo '' > ${includes}/includes.conf",
-        creates  => "${includes}/includes.conf",
-        require  => File["${includes}"],
-    } # exec
+  exec { "prep dhcp_include":
+    command  => "echo '' > ${includes}/includes.conf",
+    creates  => "${includes}/includes.conf",
+    require  => File["${includes}"],
+  } # exec
 
-    service { "dhcpd":
-        ensure  => running,
-        enable  => true,
-        require => [
-            Package["dhcp"],
-            File["/etc/dhcpd.conf"],
-            Snippet["default"],
-        ],
-    } # service
+  service { "dhcpd":
+    ensure  => running,
+    enable  => true,
+    require => [
+      Package["dhcp"],
+      File["/etc/dhcpd.conf"],
+      Snippet["default"],
+    ],
+  } # service
 
-    dhcp::snippet { "default":
-        content => template("dhcp/default_snippet.erb"),
-    } # dhcp::snippet
+  dhcp::snippet { "default":
+    content => template("dhcp/default_snippet.erb"),
+  } # dhcp::snippet
 } # class dhcp
--- manifests/snippet.pp
+++ manifests/snippet.pp
@@ -3,10 +3,11 @@
 # add a snippet to the $include directory to be built into a config
 #
 # Parameters:
-#   $present - present||absent, defaults to present
-#   $source  - specify a source file
-#   $content - specify content or a template
-#   $nofile  - creates an empty file named "$name.conf"
+#   $present   - present||absent, defaults to present
+#   $source    - specify a source file
+#   $content   - specify content or a template
+#   $nofile    - creates an empty file named "$name.conf"
+#   $dhcpnetmask - netmask of network, defaults to 255.255.255.0
 #
 # Actions:
 #   add a snippet to the $include directory to be built into a config
@@ -15,51 +16,53 @@
 #   must specify at least $source or $content
 #
 # Sample Usage:
-#    snippet { "default":
-#        content => template("dhcp/default_snippet.erb"),
+#  snippet { "default":
+#    content => template("dhcp/default_snippet.erb"),
 #}   # snippet
 #
-define dhcp::snippet ( $ensure = "present", $source = undef, $content = undef, $nofile = undef ) {
+define dhcp::snippet ( $ensure = "present", $source = undef, $content = undef, $nofile = undef, $dhcpnetmask="255.255.255.0", $template="" ) {
 
-    case $ensure {
-        present: {
-            exec {"$ensure : $name : $origins":
-                command => "sed -i '\$iinclude \"${dhcp::includes}/$name.conf\";' ${dhcp::includes}/includes.conf",
-                unless  => "grep 'include \"${dhcp::includes}/$name.conf\"' ${dhcp::includes}/includes.conf",
-                require => Exec["prep dhcp_include"],
-            } # exec
+  $filename=$dhcp::filename
 
-            if $nofile {
-                exec { "touch ${dhcp::includes}/$name.conf":
-                    creates => "${dhcp::includes}/$name.conf",
-                } # exec
-            } else {
-                case $content {
-                    '':      {
-                        file { "${dhcp::includes}/$name.conf":
-                            source  => "$source",
-                            require => File["${dhcp::includes}"],
-                        } # file
-                    } # '':
-                    default: {
-                        file { "${dhcp::includes}/$name.conf":
-                            content => "$content",
-                            require => File["${dhcp::includes}"],
-                        } # file
-                    } # default:
-                } # case $content
-            } # fi $nofile
-        } # present:
-        default: {
-            exec {"$ensure : $name : $origins":
-                command => "sed -i '/include ${dhcp::includes}/$name.conf/d' ${dhcp::includes}/includes.conf",
-                onlyif  => "grep 'include \"${dhcp::includes}/$name.conf\"' ${dhcp::includes}/includes.conf",
-                require => Exec["prep dhcp_include"],
-            } # exec
+  case $ensure {
+    present: {
+      exec {"$ensure : $name : $origins":
+        command => "sed -i '\$include \"${dhcp::includes}/$name.conf\";' ${dhcp::includes}/includes.conf",
+        unless  => "grep 'include \"${dhcp::includes}/$name.conf\"' ${dhcp::includes}/includes.conf",
+        require => Exec["prep dhcp_include"],
+      } # exec
 
+      if $nofile {
+        exec { "touch ${dhcp::includes}/$name.conf":
+          creates => "${dhcp::includes}/$name.conf",
+        } # exec
+      } else {
+        case $template{
+          '':    {
             file { "${dhcp::includes}/$name.conf":
-                ensure => absent,
+              source  => "$source",
+              require => File["${dhcp::includes}"],
             } # file
-        } # default:
-    } # case $ensure
+          } # '':
+          default: {
+            file { "${dhcp::includes}/$name.conf":
+              content => template($template),
+              require => File["${dhcp::includes}"],
+            } # file
+          } # default:
+        } # case $content
+      } # fi $nofile
+    } # present:
+    default: {
+      exec {"$ensure : $name : $origins":
+        command => "sed -i '/include ${dhcp::includes}/$name.conf/d' ${dhcp::includes}/includes.conf",
+        onlyif  => "grep 'include \"${dhcp::includes}/$name.conf\"' ${dhcp::includes}/includes.conf",
+        require => Exec["prep dhcp_include"],
+      } # exec
+
+      file { "${dhcp::includes}/$name.conf":
+        ensure => absent,
+      } # file
+    } # default:
+  } # case $ensure
 } # define dhcp::snippet
