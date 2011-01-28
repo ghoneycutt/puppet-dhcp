a2a142f2b4573a36615067db956272b8800de996
ghoneycutt <garrett@puppetlabs.com>
2011-01-27 17:18:59 -0800

    script that custom function calls

---
+++ generate_hosts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while read line
+do
+  hostname=$(echo $line | awk '{print $1}')
+  mac=$(echo $line | awk '{print $2}')
+  ip=$(echo $line | awk '{print $3}')
+
+cat << HOSTENTRY
+host $hostname {
+  hardware ethernet $mac;
+  fixed-address $ip;
+}
+HOSTENTRY
+done < $1
+exit 0
